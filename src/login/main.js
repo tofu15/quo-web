e70e7b59b1edc68b848f4ce22a4e47365a25bbe5
tofu15 <litiangang0422@protonmail.com>
2022-08-15 02:39:07 +0800

    20220814 update

--- src/login/main.js
+++ src/login/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import '@/assets/main.css'
+import '@/assets/sass/main.sass'
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const root = resolve(__dirname, 'src')
+const publicDir = resolve(__dirname, 'public')
+const outDir = resolve(__dirname, 'dist')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
+  publicDir,
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@modules': fileURLToPath(new URL('./node_modules', import.meta.url))
     }
+  },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        login: resolve(root, 'login', 'index.html')
+      }
+    }
+  },
+  css: {
+    devSourcemap: true
   }
 })
