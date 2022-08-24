09d265d66d7dee3e34e3e5ededde8306761f625d
tofu15 <litiangang0422@protonmail.com>
2022-08-24 19:02:09 +0800

    更新

---
+++ src/components/Common/CommonTable.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th colspan="2">The table header</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>The table body</td>
+                <td>with two columns</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style lang="sass" scoped>
+</style>
--- src/views/Product/ListView.vue
+++ src/views/Product/ListView.vue
@@ -1,11 +1,37 @@
 <script setup>
+import { reactive, computed } from 'vue'
+import { onBeforeRouteLeave } from 'vue-router'
+import MainViewHeader from '@/components/Common/MainViewHeader.vue';
+import CommonTable from '@/components/Common/CommonTable.vue';
+import CommonModal from '@/components/Common/CommonModal.vue';
+import router from '/router';
+// header 参数
+const headerProps = {
+    title: '製品一覧',
+    urls: [
+        {
+            text: 'ホーム',
+            isUrl: true,
+            url: '/'
+        },
+        {
+            text: ' / 製品管理',
+            isUrl: false,
+            url: ''
+        },
+        {
+            text: ' / 製品一覧',
+            isUrl: false,
+            url: ''
+        }
+    ]
+}
 </script>
-
 <template>
     <div>
-        <h1>製品一覧</h1>
+        <MainViewHeader v-bind="headerProps"></MainViewHeader>
+        <CommonTable></CommonTable>
     </div>
 </template>
-
 <style lang="sass" scoped>
 </style>
