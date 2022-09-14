cc0f4fcfd81ef71fa97ac45d9a6a9131c8f21efa
tofu15 <litiangang0422@protonmail.com>
2022-09-15 07:51:43 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import {quasar, transformAssetUrls} from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    quasar()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@modules': fileURLToPath(new URL('./node_modules', import.meta.url))
+    plugins: [
+        vue({
+            template: {transformAssetUrls}
+        }),
+        quasar()
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@modules': fileURLToPath(new URL('./node_modules', import.meta.url))
+        }
     }
-  }
 })
