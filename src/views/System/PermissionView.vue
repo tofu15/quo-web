ceacb479037109a6951cfbe181c680c557cc8414
tofu15 <litiangang0422@protonmail.com>
2022-09-20 22:48:14 +0800

    update

--- src/views/System/PermissionView.vue
+++ src/views/System/PermissionView.vue
@@ -273,9 +273,9 @@ function save() {
 <template>
     <div>
         <MainViewHeader v-bind="headerProps"></MainViewHeader>
-        <q-select v-model="form.dno" :options="depts"
+        <q-select class="input" :rules="[val => !!val || '入力必須です。']" v-model="form.dno" :options="depts"
                   label="部署" outlined emit-value map-options/>
-        <q-select v-model="form.rid"
+        <q-select class="input" :rules="[val => !!val || '入力必須です。']" v-model="form.rid"
                   :options="rolesOfDept" label="職位" outlined emit-value map-options/>
         <div v-if="!(form.rid === null || form.isGeting === true)">
             <q-checkbox v-model="permissionOfRole[0]" :label="permissionList[0].name"/>
